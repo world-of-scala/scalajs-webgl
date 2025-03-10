90fc8cbf6d314d13ee00158d2d94c77d3c4b20f9
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2025-03-10 10:06:39 +0100

    Refactor HomePage component to enhance WebGL demo navigation and update layout

--- example/client/src/main/scala/org/worldofscala/scalajswebgl/app/HomePage.scala
+++ example/client/src/main/scala/org/worldofscala/scalajswebgl/app/HomePage.scala
@@ -6,50 +6,12 @@ object HomePage:
 
   def apply(): HtmlElement =
     div(
-      cls := "wrapper",
-      div(
-        cls := "scala",
-        img(src := "img/scala.svg", width := "200px"),
-        "Scala"
-      ),
-      div(
-        cls := "jvm",
-        img(src := "img/juke.png", width := "50px"),
-        "Backend"
-      ),
-      div(
-        cls := "js",
-        img(src := "img/js.jpg", width := "150px"),
-        "Frontend"
-      ),
-      div(
-        display := "flex",
-        cls     := "server",
-        ul(
-          li("zio-logging")
-        )
-      ),
-      div(
-        alignItems := "center",
-        cls        := "shared",
-        div(
-          img(src := "img/zio.png"),
-          ul(
-            styleAttr := "float: right; clear: left; margin-right: 2em;",
-            li("Iron")
-          ),
-          ul(
-            li("zio-http"),
-            li("zio-json"),
-            li("Tapir", img(src := "img/tapir.svg", verticalAlign := "middle", width := "50px"))
-          )
-        )
-      ),
-      div(
-        display := "flex",
-        cls     := "client",
-        img(marginRight := "1em", src := "img/laminar.png", width := "50px", height := "50px"),
-        p(marginTop     := "1em", "Laminar")
+      h1("Scala.js WebGL"),
+      ul(
+        demo("WebGL Sample", Router.uiRoute("webgl")),
+        demo("Shaders", Router.uiRoute("webgl", "shaders")),
+        demo("Triangle", Router.uiRoute("webgl", "adammurray", "triangle")),
+        demo("Laminar WebGL Sample", Router.uiRoute("webgl", "detect"))
       )
     )
 
